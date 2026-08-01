28aa6d7c3a78f6c88b761198542d0a998363f76c
Floh-Joe <139984517+Floh-Joe@users.noreply.github.com>
2026-08-01 21:45:02 +0200

    Replace solar optimizer script with server.py

--- solar_optimizer_addon/run.sh
+++ solar_optimizer_addon/run.sh
@@ -3,8 +3,4 @@
 export SE_USERNAME="${SE_USERNAME}"
 export SE_PASSWORD="${SE_PASSWORD}"
 
-while true; do
-    printf "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" > /tmp/response.txt
-    python3 /app/solaredge_optimizers.py >> /tmp/response.txt
-    nc -l -p 8126 < /tmp/response.txt
-done
+python3 /app/server.py
